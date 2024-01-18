a2e02d0837a71610f7b999264a420bd235792364
landry <landryankam@gmail.com>
2024-01-18 20:26:56 +0100

    debut de l'implementation du formulaire

--- libs/home/events/src/index.ts
+++ libs/home/events/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/events/events.component';
+
+export * from './lib/event-item/event-item.component';
---
+++ libs/home/events/src/lib/event-item/event-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'd-ev-event-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './event-item.component.html',
+  styleUrl: './event-item.component.scss',
+})
+export class EventItemComponent {}
